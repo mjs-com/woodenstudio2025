44c16fa2e719583b8a859ca29b171777d9546555
mjs-com <58849118+mjs-com@users.noreply.github.com>
2025-06-25 21:32:26 +0900

    upload files
    公開用のファイルをアップロード

---
+++ js/main.js
@@ -0,0 +1,82 @@
+
+// GSAPからメディアクエリの関数を実行
+const mm = gsap.matchMedia();
+//ブレイクポイントの指定
+//端末の画面が1025px以上の場合（PC
+mm.add("(min-width:1025px)", () => {
+gsap.fromTo(
+    ".concept-first .column-left",
+    2,
+    {
+        x: -100,
+        opacity: 0
+    },
+    {
+        x: 0,
+        opacity: 1,
+        scrollTrigger: {
+            trigger: ".concept-first .column-left",
+            start: "top center",
+            markers: true
+        }
+    }
+)
+
+gsap.fromTo(
+    ".concept-first .column-right",
+    2,
+    {
+        x: 100,
+        opacity: 0
+    },
+    {
+        x: 0,
+        opacity: 1,
+        delay: 0.5,
+        scrollTrigger: {
+            trigger: ".concept-first .column-right",
+            start: "top center",
+            markers: true
+        }
+    }
+)
+})
+//端末画面幅が599px以下の時（スマホ）
+mm.add("(max-width:599px)", () => {
+    gsap.fromTo(
+        ".concept-first .column-left",
+        2,
+        {
+            y: 100,
+            opacity: 0
+        },
+        {
+            y: 0,
+            opacity: 1,
+            scrollTrigger: {
+                trigger: ".concept-first .column-left",
+                start: "top center",
+                markers: true
+            }
+        }
+    )
+    
+    gsap.fromTo(
+        ".concept-first .column-right",
+        2,
+        {
+            y: 100,
+            opacity: 0
+        },
+        {
+            y: 0,
+            opacity: 1,
+            delay: 0.5,
+            scrollTrigger: {
+                trigger: ".concept-first .column-right",
+                start: "top center",
+                markers: true
+            }
+        }
+    )
+})
